7a32a4c19c298b3482c13a9263f83a2baf641c86
olabisim <ajoseholabisi@gmail.com>
2024-03-12 01:51:14 -0700

    added models and db

---
+++ models/User.js
@@ -0,0 +1,26 @@
+import {Schema} from 'mongoose';
+
+
+const userSchema = new Schema({
+    name: {
+        type: String, 
+        unique: true,
+        required: true,
+    },
+    email: {
+        type: String, 
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String, 
+        required: true,
+    },
+},
+// it's going to crerated updated time and created time
+{timestamps: true}
+
+)
+
+
+export default mongoose.model("User", userSchema)
